34c9dd00b93bf88b3bd0952616928a48295da28c
MarkoSchoretits <marko@schoretits.com>
2022-12-02 19:33:16 +0100

    Optimized styles

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -7,25 +7,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('date')
-            ->add('description')
-            ->add('picture')
-            ->add('capacity')
-            ->add('email')
-            ->add('phone')
-            ->add('location')
-            ->add('street')
-            ->add('number')
-            ->add('zip')
-            ->add('city')
-            ->add('url')
-            ->add('type')
+            ->add('name', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('date', DateTimeType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('description', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('picture', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('capacity', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('email', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('phone', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('location', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('street', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('number', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('zip', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('city', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('url', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
+            ->add('type', TextType::class, ['attr' => ['class' => 'form-control form-control-sm bg-light']])
         ;
     }
 
